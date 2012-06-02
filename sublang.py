0de0d8183b91838d682a0e489241f5b981549b68
nick <kvouter@gmail.com>
2012-06-02 21:38:26 +0400

    replace function

--- sublang.py
+++ sublang.py
@@ -18,11 +18,13 @@ def translate(text, sourcelang, targetlang):
 		fixed_json = re.sub(r',{2,}', ',', response).replace(',]', ']')
 		data = json.loads(fixed_json)
 		sublime.status_message(data[0][0][0])
-		print data[0][0][0]
+		return data[0][0][0]
 	except (urllib2.HTTPError) as (e):  
 		sublime.error_message('%s: HTTP error %s contacting Google Translate' % (__name__, str(e.code)))  
 	except (urllib2.URLError) as (e):  
 		sublime.error_message('%s: URL error %s contacting Google Tsanslate' % (__name__, str(e.reason)))
+
+
 
 class GoolgeTsanslateCaller(threading.Thread):  
     def __init__(self, sl, tl, text, timeout):  
@@ -59,4 +61,25 @@ class TranslateCommand(sublime_plugin.TextCommand):
 		except Exception as ex:
 			sublime.message_dialog(ex.message)
 
+class TranslaterCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		try:
+			v = self.view
 
+			mes = v.substr(v.sel()[0])
+
+			str = sublime.load_settings('Preferences.sublime-settings').get("sublang");
+			
+			if str is None:
+				raise Exception, u'Erorr configuration. Not found sublang in config'
+			else:
+				langs = str.split('>')
+				if len(langs)!=2: 
+					raise Exception, u'Erorr configuration. Value sublang requre {1}>{2}'
+				else:
+					sl = langs[0].strip()
+					tl = langs[1].strip()
+					v.replace(edit, v.sel()[0], translate(mes,sl, tl))
+
+		except Exception as ex:
+			sublime.message_dialog(ex.message)
